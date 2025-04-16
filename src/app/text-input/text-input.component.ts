274493259c07a941dd660ece674baf8eaf98fc42
Edwald <edwald@cessleigh.com>
2025-04-16 11:54:19 +1000

    update password input

--- src/app/text-input/text-input.component.ts
+++ src/app/text-input/text-input.component.ts
@@ -1,11 +1,17 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, signal } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  bootstrapEyeFill,
+  bootstrapEyeSlashFill,
+} from '@ng-icons/bootstrap-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-text-input',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIcon],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.css',
+  viewProviders: [provideIcons({ bootstrapEyeFill, bootstrapEyeSlashFill })],
 })
 export class TextInputComponent {
   // @Input({ required: true }) title!: string;
@@ -15,4 +21,10 @@ export class TextInputComponent {
   fieldName = input.required<string>();
   type = input<string>('text');
   @Input({ required: true }) formGroup!: FormGroup;
+  isHidden = signal<boolean>(true);
+
+  onClick() {
+    console.log('Clicked');
+    this.isHidden.update((o) => !o);
+  }
 }
